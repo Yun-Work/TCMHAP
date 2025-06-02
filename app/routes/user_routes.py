a407826f2f25219396094bd10a9163dea9658237
Yun <wxyun19@gmail.com>
2025-06-02 21:40:02 +0800

    init

---
+++ app/routes/user_routes.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, request, jsonify
+from app.services.user_service import get_all_users, add_user
+
+user_bp = Blueprint('user_bp', __name__)
+
+@user_bp.route('/', methods=['GET'])
+def list_users():
+    return jsonify(get_all_users())
+
+@user_bp.route('/', methods=['POST'])
+def create_user():
+    data = request.json
+    new_user = add_user(data['name'], data['email'])
+    return jsonify(new_user), 201
