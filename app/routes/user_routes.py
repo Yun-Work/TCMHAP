16a22e73f9ce5b3646d09df1537ae15b1dc823a6
alanchen <alanchen@fastbridge.com.tw>
2025-06-13 16:20:55 +0800

    確認路由調整bug

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -5,7 +5,7 @@ user_bp = Blueprint('user_bp', __name__)
 
 @user_bp.route('/status', methods=['GET'])
 def status():
-    return "Python專案正常運行中", 200
+    return 200
 
 @user_bp.route('/', methods=['GET'])
 def list_users():
