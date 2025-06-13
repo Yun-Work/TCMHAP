f60b63ed2ac4aaa5233240065ccb97d3956dd035
alanchen <alanchen@fastbridge.com.tw>
2025-06-13 16:08:16 +0800

    新增確認專案運行用路由

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -3,6 +3,10 @@ from app.services.user_service import get_all_users, add_user
 
 user_bp = Blueprint('user_bp', __name__)
 
+@user_bp.route('/status', methods=['GET'])
+def list_users():
+    return "Python專案正常運行中", 200
+
 @user_bp.route('/', methods=['GET'])
 def list_users():
     return jsonify(get_all_users())
