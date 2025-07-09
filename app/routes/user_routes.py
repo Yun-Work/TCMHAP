25f4cc5ae49bc3383d59f05d3c5c8639ad03a05b
Yun <wxyun19@gmail.com>
2025-07-10 00:16:06 +0800

    增加臉部全息位置、清除障礙物的RestfulAPI註解

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -18,13 +18,12 @@ def create_user():
     data = request.json
     new_user = add_user(data['name'], data['email'])
     return jsonify(new_user), 201
-# 全息位置
+# 臉部全息位置、清除障礙物的RestfulAPI(參考此程式)
 @user_bp.route('/hologram', methods=['POST'])
 def hologram():
+    # 呼叫hologram_service的main
     result_img = main(request)
-
-    if result_img  is None:
-        return 'No face detected', 422
+    # 以下是測試用
     cv2.imshow("標記結果", result_img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
