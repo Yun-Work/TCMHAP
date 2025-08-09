389233470a876d9aa9901e71c3f8e5681b1828ae
Hannah <kelly920929@gmail.com>
2025-08-10 02:36:54 +0800

    連接前端測試

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 from app.routes.hologram_routes import hologram_bp
 from app.routes.user_routes import user_bp
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     app.register_blueprint(user_bp, url_prefix='/api/users')
     app.register_blueprint(hologram_bp, url_prefix='/api/holograms')
+
+    @app.get("/status")
+    def status():
+        return jsonify({"ok": True}), 200
+
     return app
 
